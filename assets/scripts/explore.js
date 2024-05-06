398ce3e4d53faa91fe671608a0b60cb9c93f6842
tirani427 <tirani@ucsd.edu>
2024-05-05 17:56:52 -0700

    editing expose.html and expose.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,13 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const button = document.getElementById("start-talking");
+  button.addEventListener("click", () => smilingFace());
+
+  const voiceSelector = document.getElementById("voice-select");
+}
+
+function smilingFace(){
+  const picture = document.getElementById("smiling-face");
+  picture.src = "assets/images/smiling-open.png";
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,60 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  let audio_sound = document.querySelector('#audio');
+  const hornSelect = document.getElementById("horn-select");
+  hornSelect.addEventListener("change", () => hornSelected(hornSelect.value, audio_sound));
+
+  const volume_slider = document.getElementById("volume");
+  volume_slider.addEventListener("mouseup", () => audioVolumeChanged(volume_slider.value, volume_slider.max, audio_sound))
+
+  //const audio = document.getElementsByClassName("hidden");
+  const btn_play_audio = document.getElementById("play-sound");
+  btn_play_audio.addEventListener("click", () => playAudio(audio_sound));
+}
+
+function hornSelected(selected_value, audio) {
+  //const optionSelected = hornSelect.value;
+  const picture = document.getElementById("display-picture");
+  //const audio = document.getElementById("aud-sound");
+  //picture.src = "assets/images/air-horn.svg";
+  if(selected_value == "air-horn"){
+    picture.src = "assets/images/air-horn.svg";
+    audio.src = "assets/audio/air-horn.mp3";
+  }
+  else if(selected_value == "car-horn"){
+    picture.src = "assets/images/car-horn.svg";
+    audio.src = ("assets/audio/car-horn.mp3");
+  }
+  else if(selected_value == "party-horn"){
+    picture.src = "assets/images/party-horn.svg";
+    audio.src = ("assets/audio/party-horn.mp3");
+  }
+}
+
+function audioVolumeChanged(changed_value, max_value, audio){
+  const audio_picture = document.getElementById("volume-picture");
+  //const audio_volume = document.getElementById("aud-sound");
+  let newVolume = (changed_value)/100;
+  if(changed_value == 0){
+    audio_picture.src = "assets/icons/volume-level-0.svg";
+    audio.volume = 0;
+  }
+  else if(changed_value >= 1 && changed_value < 33){
+    audio_picture.src = "assets/icons/volume-level-1.svg";
+    audio.volume = newVolume;
+  }
+  else if(33 <= changed_value && changed_value < 67){
+    audio_picture.src = "assets/icons/volume-level-2.svg";
+    audio.volume = newVolume;
+  }
+  else if(67 <= changed_value && changed_value <= max_value){
+    audio_picture.src = "assets/icons/volume-level-3.svg";
+    audio.volume = newVolume;
+  }
+}
+
+function playAudio(audio){
+  audio.play();
 }
