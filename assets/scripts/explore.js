c35a61ae5cedbf00ec533b41c310b9954347c58b
tirani427 <tirani@ucsd.edu>
2024-05-07 14:07:06 -0700

    finished explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -10,10 +10,12 @@ function init() {
   button.addEventListener("click", () => smilingFace());
 
   const voiceSelector = document.getElementById("voice-select");
-
   const voice_options = document.getElementById("voice-select");
 
-  addVoices(voice_options);
+  voice_options.addEventListener("click", () => populateVoices());
+
+  //voice_options.options[voice_options.options.length] = new Option('Text 1', 'Value1');
+
 
   const speakingText = document.getElementById("text-to-speak");
 
@@ -25,27 +27,108 @@ function init() {
 function smilingFace(){
   const picture = document.getElementById("smiling-face");
   picture.src = "assets/images/smiling-open.png";
+
+  speakingText();
+
+  picture.src = "assets/images/smiling.png";
 }
 
-function addVoices(selector_list){
-  let voices = voice_source.getVoices();
+/*function populateVoices() {
+  document.getElementById("voice-selector").options[document.getElementById("voice-selector").options.length] = new Option('Text 1', 'Value1');
+}*/
 
-  for(let i = 0; i < voices.length; i++){
+
+
+
+function speakingText() {
+  const speak_text = document.getElementById("text-to-speak").value;
+  //const utterance = new SpeechSynthesisUtterance(speak_text);
+  if(speak_text == ''){
+    return;
+  }
+  const selectOption = document.getElementById("voice-select").value;
+  if(selectOption == "select"){
+    return;
+  }
+  const utterThis = new SpeechSynthesisUtterance(speak_text);
+  const selectedOption =
+    document.getElementById("voice-select").selectedOptions[0].getAttribute("data-name");
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+  synth.speak(utterThis);
+  speak_text.blur();
+  return;
+
+}
+
+
+const synth = window.speechSynthesis;
+
+const inputForm = document.querySelector("form");
+const inputTxt = document.querySelector(".txt");
+const voiceSelect = document.querySelector("select");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector(".pitch-value");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector(".rate-value");
+
+let voices = [];
+
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
     const option = document.createElement("option");
-    option.textContent = `${voices[i].name} (${vocies[i].lang})`;
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
 
-    if(voices[i].default){
-      option.textContent = `${voices[i].name} (${vocies[i].lang}) -- DEFAULT`;
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
     }
 
     option.setAttribute("data-lang", voices[i].lang);
     option.setAttribute("data-name", voices[i].name);
-
-    selector_list.appendChild(option);
-
+    voiceSelect.appendChild(option);
   }
 }
 
-function speakingText(){
-
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
 }
+
+inputForm.onsubmit = (event) => {
+  event.preventDefault();
+
+  const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute("data-name");
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+  utterThis.pitch = pitch.value;
+  utterThis.rate = rate.value;
+  synth.speak(utterThis);
+
+  inputTxt.blur();
+};
+
+
+input.onsubmit = (event) => {
+  event.preventDefault();
+
+  const utterThis = new SpeechSynthesisUtterance(speak_text);
+  const selectedOption =
+    document.getElementById("voice-select").selectedOptions[0].getAttribute("data-name");
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+  synth.speak(utterThis);
+  inputTxt.blur();
+};
